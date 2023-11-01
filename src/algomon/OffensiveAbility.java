022554e8a59e342d623303938dcc852ae9db39a6
2022503 <146199452+2022503@users.noreply.github.com>
2023-11-01 14:02:45 +0000

    an OffensiveAbility is added
    added offensive class

--- src/algomon/OffensiveAbility.java
+++ src/algomon/OffensiveAbility.java
@@ -8,6 +8,21 @@ package algomon;
  *
  * @author User
  */
-public class OffensiveAbility {
+public class OffensiveAbility implements abilityTtype {
     
+    
+    private String name;
+    
+    public OffensiveAbility(String name){
+        
+        this.name = name;
+    }
+    
+    @Override
+    
+    public void activate(Algomon source, Algomon target){
+    
+        System.out.println(source +" activates Offensive Ability"" + name +""on" + target.name);
+    
+    }
 }
