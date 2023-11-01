528e71d927ebc45effa6571e91f42f03d53c0ba3
2022179 <146199008+2022179@users.noreply.github.com>
2023-11-01 14:00:32 +0000

    updated the match class

--- src/algomon/Match.java
+++ src/algomon/Match.java
@@ -10,4 +10,19 @@ package algomon;
  */
 public class Match {
     
+private Player player1;
+private Player player2;
+
+//code for players 
+public Match(Player player1, Player player2) {
+this.player1 = player1;
+this.player2 = player2;
 }
+
+public void startMatch() {
+    //output
+System.out.println("match just started !");
+}
+}
+    
+
