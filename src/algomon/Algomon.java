022a8f57fc8fc81fa8a74d4f112d9bf49f3e8f06
2022179 <146199008+2022179@users.noreply.github.com>
2023-11-01 13:27:40 +0000

    created method size on algomon team

--- src/algomon/Algomon.java
+++ src/algomon/Algomon.java
@@ -24,35 +24,14 @@ public class Algomon {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-
-    private final String name;
-    private final ElementType element;
-    private final int health;
-    private final int attack;
-    private final int defense;
-    private final int speed;
-    private final ArrayList<Object> abilities;
-    public Algomon(String name, ElementType element)
-    {
-this.name = name;
-this.element = element;
-this.health = new Random().nextInt(51) + 50;
-
-this.attack = new Random().nextInt(21) + 10;
-this.defense = new Random().nextInt(16) + 5;
-this.speed = new Random().nextInt(10) + 1;
-this.abilities = new ArrayList<>();
-}
-    
-    public void addAbility(Ability ability) {
-abilities.add(ability);
-}
-
-public void activateAbility(int index, Algomon target) {
-if (index &gt;= 0 &amp;&amp; index &lt; abilities.size()) {
-abilities.get(index).activate(this, target);
-}
-}
+      final String name;
+     final  String type;
+      final int health;
+      final int attack;
+     final int defense;
+      final int speed;
+      ArrayList<ability>[] abilities;    
 
 }
+  
 }
